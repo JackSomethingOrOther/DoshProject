6d2704467798d41494290df8f14a3d08e559c9f8
Jack Somethingorother <Doomguy127@gmail.com>
2018-09-04 20:16:13 -0500

    Add project files.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class MoneyValue
+    {
+        public int Dollars { get; set; }
+        public int Cents { get; set; }
+
+        public MoneyValue(decimal Money)
+        {
+            decimal Test = Math.Truncate(Money);
+            Cents = (int)(((Money - Test) * 100));
+            Dollars = (int)Test;
+        }
+
+        public void IncrementMoney()
+        {
+
+        }
+
+        public void DecrementMoney()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneyValue moneyValueObject = new MoneyValue(10.75m);
+            Console.WriteLine("Cents: {0}, Dollars: {1}", moneyValueObject.Cents, moneyValueObject.Dollars);
+            Console.ReadLine();
+        }
+    }
+}
